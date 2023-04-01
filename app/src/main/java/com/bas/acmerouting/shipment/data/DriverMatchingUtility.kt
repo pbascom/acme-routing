ee86663ae3aeb5395091cb404ee7a08b21adf019
phillip.bascom@gmail.com <phil@fancy-mac.lan>
2023-04-01 15:59:54 -0400

    add business logic

--- app/src/main/java/com/bas/acmerouting/shipment/data/DriverMatchingUtility.kt
+++ app/src/main/java/com/bas/acmerouting/shipment/data/DriverMatchingUtility.kt
@@ -1,18 +1,149 @@
 package com.bas.acmerouting.shipment.data
 
 import java.lang.Integer.min
+import java.util.*
 
 /**
  * Adapts the json structure specified by the coding assignment into a Map of (Driver, Route) pairs.
  * See [ShipmentRepository] kdoc for an explanation of the motivation behind this class.
  */
 class DriverMatchingUtility {
-    fun adapt(response: ShipmentResponse) : Map<Driver, Route> {
-        val shipments = hashMapOf<Driver, Route> ()
-        // TODO: Implement business logic rules
-        for(index in 0 until min(response.drivers.size, response.routes.size)) {
-            shipments[Driver(response.drivers[index])] = Route(response.routes[index])
+    fun adapt(response: ShipmentResponse): Map<Driver, Route> {
+        val shipments = hashMapOf<Driver, Route>()
+        // There are two parts to this assignment. First, we need to determine each driver's
+        // suitability to take each route, using the following rules:
+
+        // If the length of the shipment's destination street name is even, the base suitability
+        // score is the number of vowels in the driver's name multiplied by 1.5
+
+        // If the length of the shipment's destination street name is odd, the base suitability
+        // score is the number of consonants in the driver's name multiplied by 1
+
+        // If the length of the shipment's destination street name shares any common factors (other
+        // than 1) with the length of the driver's name, the SS is increased by 50% above the base SS.
+
+        // Next, we must assign drivers to routes which maximize the total suitability over the
+        // entire dataset.
+
+        // Computing an SS for each driver / destination pair is O(n^2) by itself, and I don't
+        // immediately see a matching algorithm that doesn't involve a huge number of comparisons
+        // as well. But, may as well get started.
+
+        // We'll start by pulling the relevant metrics out of each data set.
+        val analyzedDrivers = response.drivers.map { driver -> AnalyzedDriver.create(driver) }
+        val analyzedDestinations = response.routes.map { destination -> AnalyzedDestination.create(destination) }
+
+        // So, if I understand the problem correctly, we would need to check every possible
+        // combination of driver / destination pairs to guarantee we get the maximum value. That may
+        // even be possible with a data set this small, but there's no way it scales to anything even
+        // approaching real numbers, so I won't do it here. Instead, I'm going to implement a simple
+        // greedy algorithm which will add the highest SS pair, and then the highest remaining pair,
+        // and so on. This algorithm is vulnerable to getting stuck in local minima, but I'm an
+        // Android engineer not a Graph Theorist.
+
+        // We'll start by calculating every Suitability Score and putting them in a priority queue.
+        // By default, the Priority Queue puts the smallest priority on top, so we invert the
+        // comparator.
+        val suitabilityComparator: Comparator<Suitability> = compareBy { it.score * -1 }
+        val suitabilityQueue: PriorityQueue<Suitability> = PriorityQueue(suitabilityComparator)
+
+        for (driver in analyzedDrivers) {
+            for (destination in analyzedDestinations) {
+                suitabilityQueue.add(Suitability(
+                    driver,
+                    destination,
+                    computeSuitabilityScore(driver, destination))
+                )
+            }
         }
+
+        // Now, we make hashSets of pre-assigned drivers and destinations for that sweet, sweet
+        // O(1) member access time.
+        val assignedDrivers = hashSetOf<AnalyzedDriver>()
+        val assignedDestinations = hashSetOf<AnalyzedDestination>()
+
+        // And finally, we iterate through the Priority Queue until it's empty, taking the highest
+        // unassigned pair and adding it to the shipments Map.
+        while(!suitabilityQueue.isEmpty()) {
+            val mostSuitableMatch = suitabilityQueue.poll() ?: continue
+            if(assignedDrivers.contains(mostSuitableMatch.driver)) continue
+            if(assignedDestinations.contains(mostSuitableMatch.destination)) continue
+
+            assignedDrivers.add(mostSuitableMatch.driver)
+            assignedDestinations.add(mostSuitableMatch.destination)
+            shipments[Driver(mostSuitableMatch.driver.name)] = Route(mostSuitableMatch.destination.destination)
+        }
+
         return shipments
+    }
+
+    private fun computeSuitabilityScore(driver: AnalyzedDriver, destination: AnalyzedDestination): Float {
+        val baseSuitabilityScore = when(destination.streetNameLength %2 == 0) {
+            true -> 1.5f * driver.vowelCount
+            false -> driver.consonantCount.toFloat()
+        }
+
+        var hasCommonFactor = false
+        var factorToCheck = 2
+        while( !hasCommonFactor
+            && factorToCheck <= driver.nameLength/2
+            && factorToCheck <= destination.streetNameLength/2
+        ) {
+            if(driver.nameLength % factorToCheck == 0
+                && destination.streetNameLength % factorToCheck == 0
+            ) {
+                hasCommonFactor = true
+            } else {
+                factorToCheck++
+            }
+        }
+
+        return when(hasCommonFactor) {
+            true -> 1.5f * baseSuitabilityScore
+            false -> baseSuitabilityScore
+        }
+    }
+
+    data class AnalyzedDestination(
+        val streetNameLength: Int,
+        val destination: String
+    ) {
+        companion object {
+            fun create(routeDestination: String): AnalyzedDestination {
+                return AnalyzedDestination(
+                    streetNameLength = routeDestination.count { !it.isWhitespace() && !it.isDigit() },
+                    destination = routeDestination
+                )
+            }
+        }
+    }
+
+    data class AnalyzedDriver(
+        val nameLength: Int,
+        val vowelCount: Int,
+        val consonantCount: Int,
+        val name: String
+    ) {
+        companion object {
+            fun create(driverName: String): AnalyzedDriver {
+                val strippedName = driverName.filterNot { it.isWhitespace() }
+                return AnalyzedDriver(
+                    nameLength = strippedName.length,
+                    vowelCount = strippedName.count { VOWELS.contains(it.lowercaseChar()) },
+                    consonantCount = strippedName.count { !VOWELS.contains(it.lowercaseChar()) },
+                    name = driverName
+                )
+            }
+        }
+    }
+
+    data class Suitability(
+        val driver: AnalyzedDriver,
+        val destination: AnalyzedDestination,
+        val score: Float
+    )
+
+    companion object {
+        private val VOWELS = hashSetOf('a', 'e', 'i', 'o', 'u')
     }
 }
