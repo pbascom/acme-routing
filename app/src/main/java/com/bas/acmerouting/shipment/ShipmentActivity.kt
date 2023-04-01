3fb0dc321617277e9de07e9474b34897a6bc4bd6
phillip.bascom@gmail.com <phil@fancy-mac.lan>
2023-04-01 13:26:54 -0400

    Hook the ViewModel up to the UI

--- app/src/main/java/com/bas/acmerouting/shipment/ShipmentActivity.kt
+++ app/src/main/java/com/bas/acmerouting/shipment/ShipmentActivity.kt
@@ -3,10 +3,10 @@ package com.bas.acmerouting.shipment
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -14,6 +14,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.bas.acmerouting.shipment.data.Driver
+import com.bas.acmerouting.shipment.data.Route
+import com.bas.acmerouting.shipment.viewmodel.ShipmentsViewModel
 import com.bas.acmerouting.theme.AcmeRoutingTheme
 
 /**
@@ -21,34 +25,54 @@ import com.bas.acmerouting.theme.AcmeRoutingTheme
  * See [com.bas.acmerouting.launcher.LauncherActivity] for an explanation.
  */
 class ShipmentActivity : ComponentActivity() {
+    private val viewModel: ShipmentsViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AcmeRoutingTheme {
-                ShipmentUi()
+
+        viewModel.refreshShipments()
+
+        viewModel.shipmentsData.observe(this) { shipments ->
+            setContent {
+                AcmeRoutingTheme {
+                    ShipmentUi(shipments)
+                }
             }
         }
     }
 }
 
 @Composable
-fun ShipmentUi() {
+fun ShipmentUi(shipments: Map<Driver, Route>) {
     Surface(
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colors.background
     ) {
         Column(
             modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
+            verticalArrangement = Arrangement.Top,
         )
         {
             Text(
                 modifier = Modifier.fillMaxWidth(),
                 textAlign = TextAlign.Center,
-                text ="This is the router screen"
+                text = "This is the router screen"
             )
+            LazyColumn(
+                modifier = Modifier.fillMaxWidth(),
+                contentPadding = PaddingValues(8.dp)
+            ) {
+                itemsIndexed(shipments.entries.toList()) { _, entry ->
+                    ShipmentItem(driver = entry.key, route = entry.value)
+                }
+            }
         }
     }
+}
+
+@Composable
+fun ShipmentItem(driver: Driver, route: Route) {
+    Text("${driver.name} should go to ${route.destination}")
 }
 
 @Preview(
@@ -59,6 +83,13 @@ fun ShipmentUi() {
 @Composable
 fun DefaultPreview2() {
     AcmeRoutingTheme {
-        ShipmentUi()
+        ShipmentUi(
+            hashMapOf(
+                Driver("The Muffin Man") to Route("123 Drury Lane"),
+                Driver("Big Bird") to Route("Sesame Street"),
+                Driver("Harry Potter") to Route("#12 Grimmauld Place"),
+                Driver("Owl") to Route("Piglet's Old Place")
+            )
+        )
     }
 }
